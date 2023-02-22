021429702a6f406588cd6ed2720906b60a4df888
Lucy <b237237@gmail.com>
2023-02-22 21:22:40 +0800

    0.02

--- gusses.py
+++ gusses.py
@@ -1,12 +1,19 @@
 import random
 
-r = random.randint(1,100)
+start = int(input("請輸入起始值"))
+end = int(input("請輸入結束值"))
+r = random.randint(start, end)
+
+count = 0
 while True:
-    a = int(input("請輸入1~100來猜數字: "))
+    count += 1
+    a = int(input("請輸入數字來猜答案: "))
     if a == r:
         print("恭喜你猜到了")
+        print("這是你猜的第{}次".format(count))    
         break
     elif a > r:
         print("數字比答案大")
     elif a < r:
-        print("數字比答案小")      
+        print("數字比答案小")    
+    print("這是你猜的第{}次".format(count))    
